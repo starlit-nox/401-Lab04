623d728de21b1cddce0aed641724855361d2b5c6
starlit-nox <kdkelly.1998@gmail.com>
2023-07-17 13:44:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _401_Lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
