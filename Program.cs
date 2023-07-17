b925e22240ac2997e0b3c5aaeec94a959da12dcc
starlit-nox <kdkelly.1998@gmail.com>
2023-07-17 13:50:56 -0500

    Starter Code Added

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace _401_Lab04
+﻿namespace Lab04_TicTacToe
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
         }
+
+        static void StartGame()
+        {
+            // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
+
+        }
+
+
     }
 }
